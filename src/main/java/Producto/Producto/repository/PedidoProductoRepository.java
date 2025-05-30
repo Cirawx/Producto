552c354268a81eaaae0a6ed18cf7f16ad4006d08
Camilo Ignacio <167924286+Cirawx@users.noreply.github.com>
2025-05-30 03:26:18 -0400

    Update PedidoProductoRepository.java

--- src/main/java/Producto/Producto/repository/PedidoProductoRepository.java
+++ src/main/java/Producto/Producto/repository/PedidoProductoRepository.java
@@ -1,5 +1,5 @@
 package Producto.Producto.repository;
-
+//coneccion jpa
 import Producto.Producto.model.PedidoProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
